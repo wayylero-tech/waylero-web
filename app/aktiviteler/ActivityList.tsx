fe606a220d3a535638af7eacb9aacdb679250ff0
Emrah Tastan <wayylero@gmail.com>
2026-04-10 16:33:36 +0300

    cms ignore

--- app/aktiviteler/ActivityList.tsx
+++ app/aktiviteler/ActivityList.tsx
@@ -51,10 +51,7 @@ export default function ActivityList({ initialEvents, initialCityName, lang = "t
   const router = useRouter();
   const pathname = usePathname();
 
-  const cityMap: { [key: string]: number } = {
-    "ADANA": 1, "ANKARA": 7, "ANTALYA": 8, "İSTANBUL": 40, "İZMİR": 41, "KONYA": 53, "BURSA": 21, "ESKİŞEHİR": 32
-  };
-
+  const cityMap: { [key: string]: number } = { "ADANA": 1, "ADIYAMAN": 2, "AFYON": 3, "AFYONKARAHİSAR": 85, "AĞRI": 4, "AKSARAY": 5, "AMASYA": 6, "ANKARA": 7, "ANTALYA": 8, "ARDAHAN": 9, "ARTVİN": 10, "AYDIN": 11, "BALIKESİR": 12, "BARTIN": 13, "BATMAN": 14, "BAYBURT": 15, "BİLECİK": 16, "BİNGÖL": 17, "BİTLİS": 18, "BOLU": 19, "BURDUR": 20, "BURSA": 21, "ÇANAKKALE": 22, "ÇANKIRI": 23, "ÇORUM": 24, "DENİZLİ": 25, "DİYARBAKIR": 26, "DÜZCE": 27, "EDİRNE": 28, "ELAZIĞ": 29, "ERZİNCAN": 30, "ERZURUM": 31, "ESKİŞEHİR": 32, "GAZİANTEP": 33, "GİRESUN": 34, "GÜMÜŞHANE": 35, "HAKKARİ": 36, "HATAY": 37, "IĞDIR": 38, "ISPARTA": 39, "İSTANBUL": 40, "İZMİR": 41, "KAHRAMANMARAŞ": 42, "KARABÜK": 43, "KARAMAN": 44, "KARS": 45, "KASTAMONU": 46, "KAYSERİ": 47, "KİLİS": 51, "KIRIKKALE": 48, "KIRKLARELİ": 49, "KIRŞEHİR": 50, "KKTC": 84, "KOCAELİ": 52, "KONYA": 53, "KÜTAHYA": 54, "LEFKOŞA": 83, "MALATYA": 55, "MANİSA": 56, "MARDİN": 57, "MERSİN": 58, "MUĞLA": 59, "MUŞ": 60, "NEVŞEHİR": 61, "NİĞDE": 62, "ORDU": 63, "OSMANİYE": 64, "RİZE": 65, "SAKARYA": 66, "SAMSUN": 67, "ŞANLIURFA": 71, "SİİRT": 68, "SİNOP": 69, "ŞIRNAK": 72, "SİVAS": 70, "TEKİRDAĞ": 73, "TOKAT": 74, "TRABZON": 75, "TUNCELİ": 76, "UŞAK": 77, "VAN": 78, "YALOVA": 79, "YOZGAT": 80, "ZONGULDAK": 81 }
   const formatEvents = (rawItems: any[]) => {
     return rawItems.map((item: any) => {
       const rawDate = item.start || item.start_date || item.baslangic;
