c3d7546b0129d25488d52cd84414504b7ec782d0
Emrah Tastan <wayylero@gmail.com>
2026-04-14 17:43:56 +0300

    cms ignore

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,22 +1,26 @@
 "use client";
 
 import Script from "next/script";
-import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const GA_ID = "G-XC5RZVYFEF";
 
 export default function GoogleAnalytics() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
   useEffect(() => {
-    const url = pathname + (searchParams?.toString() ? `?${searchParams}` : "");
+    // SAYFA AÇILINCA ZORLA EVENT GÖNDER
+    const sendPageView = () => {
+      if (window.gtag) {
+        window.gtag("event", "page_view", {
+          page_path: window.location.pathname,
+          page_location: window.location.href,
+          page_title: document.title,
+        });
+      }
+    };
 
-    window.gtag?.("config", GA_ID, {
-      page_path: url,
-    });
-  }, [pathname, searchParams]);
+    // biraz gecikmeli çalıştır (script load için)
+    setTimeout(sendPageView, 1000);
+  }, []);
 
   return (
     <>
@@ -24,13 +28,13 @@ export default function GoogleAnalytics() {
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
         strategy="afterInteractive"
       />
+
       <Script id="ga-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           window.gtag = gtag;
           gtag('js', new Date());
-          gtag('config', '${GA_ID}');
         `}
       </Script>
     </>
