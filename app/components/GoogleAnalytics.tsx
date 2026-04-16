1307dcddb6017112aa58d80d6867926287e240f7
Emrah Tastan <wayylero@gmail.com>
2026-04-16 18:27:29 +0300

    cms ignore

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,24 +1,44 @@
 "use client";
 
 import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const GA_ID = "G-XC5RZVYFEF";
 
 export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   useEffect(() => {
+    const url = pathname + "?" + searchParams.toString();
+
     if (typeof window.gtag !== "undefined") {
-      window.gtag("js", new Date());
       window.gtag("config", GA_ID, {
-        page_path: window.location.pathname,
+        page_path: url,
       });
     }
-  }, []);
+  }, [pathname, searchParams]);
 
   return (
-    <Script
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `,
+        }}
+      />
+    </>
   );
 }
