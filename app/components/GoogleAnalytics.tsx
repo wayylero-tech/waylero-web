8307a8a90976367a5d1cf3a386bab33a49a6aee3
Emrah Tastan <wayylero@gmail.com>
2026-04-16 18:17:36 +0300

    cms ignore

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,10 +1,25 @@
 "use client";
 
 import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const GA_ID = "G-XC5RZVYFEF";
 
 export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + "?" + searchParams.toString();
+
+    if (typeof window.gtag !== "undefined") {
+      window.gtag("config", GA_ID, {
+        page_path: url,
+      });
+    }
+  }, [pathname, searchParams]);
+
   return (
     <>
       <Script
@@ -18,10 +33,9 @@ export default function GoogleAnalytics() {
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
             gtag('js', new Date());
-            gtag('config', '${GA_ID}', {
-              page_path: window.location.pathname,
-            });
+            gtag('config', '${GA_ID}');
           `,
         }}
       />
